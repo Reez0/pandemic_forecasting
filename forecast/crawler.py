805ab3afe3caec86bedbb3d2d148d15f50f777e7
reez0 <riyaazfakier2@gmail.com>
2023-09-18 14:07:42 +0200

    - Generate plot png and csv output - Cleaned up code

--- forecast/crawler.py
+++ forecast/crawler.py
@@ -1,13 +1,57 @@
+from datetime import timedelta
 from bs4 import BeautifulSoup
 import requests
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 
-def t():
+def model() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    dates, cases = retrieve_data()
+    data = {'Date': dates, 'Cases': cases}
+    df = pd.DataFrame(data)
+    df["Date"] = pd.to_datetime(df['Date'])
+    df['Cases'] = pd.to_numeric(df['Cases'], errors='coerce')
+    df = df.sort_values(by='Date')
+    df.set_index('Date', inplace=True)
+    model = sm.tsa.ARIMA(df['Cases'], order=(1, 1, 1))
+    result = model.fit()
+    last_date = df.index[-1]
+    forecast_days = 7
+
+    # Create a list of date steps for forecasting
+    date_steps = [last_date + timedelta(days=i)
+                  for i in range(1, forecast_days + 1)]
+
+    # Forecast using ARIMA
+    forecast = result.forecast(steps=forecast_days)
+
+    forecast_df = pd.DataFrame(
+        {'date': date_steps, 'new_cases': round(forecast)})
+
+    forecast_df.set_index('date', inplace=True)
+    return df, forecast_df
+
+
+def report():
+    original_dataframe, forecast = model()
+
+    forecast.to_csv('results.csv')
+
+    # Plot the actual and predicted values
+    plt.plot(original_dataframe.index,
+             original_dataframe['Cases'], label='Actual')
+    plt.plot(pd.date_range(
+        start=original_dataframe.index[-1], periods=7), forecast, label='Forecast', linestyle='--')
+    plt.legend()
+    plt.xlabel('Date')
+    plt.ylabel('Cases')
+    plt.savefig('plot.png')
+
+
+def retrieve_data() -> Tuple[List[str], List[str]]:
     response = requests.get(
         "https://www.worldometers.info/coronavirus/country/south-africa/")
     soup = BeautifulSoup(response.content, features="html.parser")
@@ -33,32 +77,8 @@ def t():
     for element in date_elements:
         dates.append(element.get('data-date'))
 
-    # # Merge dates and cases into list of dictionaries for easier handling
-    # data_list: List[Dict[str, str]] = [{'date': date, 'cases': int(
-    #     case)} for date, case in zip(dates, cases)]
-    # print(data_list)
-
-    data = {'Date': dates, 'Cases': cases}
-    df = pd.DataFrame(data)
-    df["Date"] = pd.to_datetime(df['Date'])
-    df['Cases'] = pd.to_numeric(df['Cases'], errors='coerce')
-    df = df.sort_values(by='Date')
-    df.set_index('Date', inplace=True)
-    model = sm.tsa.ARIMA(df['Cases'], order=(1, 1, 1))
-    result = model.fit()
-    forecast = result.forecast(steps=7)
-
-    # Print the forecast
-    print(f'Predicted cases for the next 7 days: {forecast}')
-
-    # Plot the actual and predicted values
-    plt.plot(df.index, df['Cases'], label='Actual')
-    plt.plot(pd.date_range(
-        start=df.index[-1], periods=7), forecast, label='Forecast', linestyle='--')
-    plt.legend()
-    plt.xlabel('Date')
-    plt.ylabel('Cases')
-    plt.savefig('foo.png')
+    return dates, cases
 
 
-t()
+if __name__ == '__main__':
+    report()
